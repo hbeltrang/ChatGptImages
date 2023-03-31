9ff1f33aaf15d644cb2314f9fc073b3cc031d2c4
hbeltrang <hbeltrang@gmail.com>
2023-03-31 15:54:35 -0600

    v1.0

---
+++ ChatGptImages/Controllers/GenerateImagesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI;
+
+namespace ChatGptImages.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerateImagesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public GenerateImagesController(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GenerateImages(string title, int qty)
+        {
+            List<string> imagesList = new List<string>();
+
+            var token = _configuration.GetValue<string>("ChatGpt:Token");
+
+            var openAIClient = new OpenAIClient(new OpenAIAuthentication(token));
+            var imageResult = await openAIClient.ImagesEndPoint.GenerateImageAsync(title, qty, OpenAI.Images.ImageSize.Medium);
+            foreach (var image in imageResult)
+            {
+                imagesList.Add(image);
+            }
+
+            return Ok(imagesList);
+        }
+    }
+}
